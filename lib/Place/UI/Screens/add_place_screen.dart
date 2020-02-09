bfe9f860a4621e6c03182e1ad491731067ef1174
DarkMarksDoe <darkmarksdoe@gmail.com>
2020-02-08 22:22:15 -0600

    Inputs from ADD PLACE

--- lib/Place/UI/Screens/add_place_screen.dart
+++ lib/Place/UI/Screens/add_place_screen.dart
@@ -2,7 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/Place/UI/Widgets/text_input_location.dart';
 import 'package:platzi_trips_app/Widgets/gradient_back.dart';
+import 'package:platzi_trips_app/Widgets/text_input.dart';
+import 'package:platzi_trips_app/Widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   File image;
@@ -16,26 +19,81 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
+    final _controllerTitlePlace = TextEditingController();
+
+    final _controllerDescriptionPlace = TextEditingController();
+
     return Scaffold(
-      body: Stack(children: <Widget>[
-        GradientBack(height: 300.0),
-        Row(children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 25.0, left: 5.0),
-            child: SizedBox(
-              height: 45.0,
-              width: 45.0,
-              child: IconButton(
-                icon: Icon(Icons.keyboard_arrow_left,
-                    color: Colors.white, size: 45),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+      body: Stack(
+        children: <Widget>[
+          GradientBack(
+            height: 300.0,
+          ),
+          Row(
+            //App Bar
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 25.0, left: 5.0),
+                child: SizedBox(
+                  height: 45.0,
+                  width: 45.0,
+                  child: IconButton(
+                      icon: Icon(
+                        Icons.keyboard_arrow_left,
+                        color: Colors.white,
+                        size: 45,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                ),
               ),
+              Flexible(
+                  child: Container(
+                padding: EdgeInsets.only(top: 45.0, left: 20.0, right: 10.0),
+                child: TitleHeader(title: "Add a new Place"),
+              ))
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom: 20.0),
+            child: ListView(
+              children: <Widget>[
+                Container(), //Foto
+                Container(
+                  //TextField Title
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: "Title",
+                    inputType: null,
+                    maxLines: 1,
+                    controller: _controllerTitlePlace,
+                  ),
+                ),
+                Container(
+                  //TextField Title
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: "Description",
+                    inputType: TextInputType.multiline,
+                    maxLines: 4,
+                    controller: _controllerTitlePlace,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInputLocation(
+                    iconData: Icons.location_on,
+                    controller: null,
+                    hintText: "Add location",
+                  ),
+                )
+
+              ],
             ),
           )
-        ])
-      ]),
+        ],
+      ),
     );
   }
 }
